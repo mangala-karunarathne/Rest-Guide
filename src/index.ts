34e10325a53c61e3422ce9bdedf4856b5beda1ae
mangala-karunarathne <mangalaicc@gmail.com>
2023-12-11 20:51:57 +0530

    CHG | Database Connection

--- src/index.ts
+++ src/index.ts
@@ -4,6 +4,7 @@ import http from "http";
 const bodyParser = require("body-parser");
 const cookieParser = require("cookie-parser");
 const compression = require("compression");
+import mongoose from "mongoose";
 
 const app = express();
 
@@ -20,6 +21,14 @@ app.use(bodyParser.json());
 const server = http.createServer(app);
 
 server.listen(8080, () => {
-    console.log("Server is running on https://localhost:8080/");
-    
-})
+  console.log("Server is running on https://localhost:8080/");
+});
+
+const MONGO_URL =
+  "mongodb+srv://mangalaicc:5jENCAys6poqBH8w@rest-api-ts.wlbf0a3.mongodb.net/?retryWrites=true&w=majority";
+
+mongoose.Promise = Promise;
+
+mongoose.connect(MONGO_URL);
+
+mongoose.connection.on("error", (error: Error) => console.log(error));
