888d32c06ed01717a154b9e24b1922a0ca578d26
mangala-karunarathne <mangalaicc@gmail.com>
2023-12-13 22:42:20 +0530

    CHG | Add user delete and user update functions

--- src/db/users.ts
+++ src/db/users.ts
@@ -20,4 +20,6 @@ export const getUserBySessionToken = (sessionToken: string) =>
   });
 export const getUserById = (id: string) => UserModel.findById(id);
 export const createUser = (values: Record<string, any>) => new UserModel(values).save().then((user)=>user.toObject());
+export const deleteUserById = (id: string) => UserModel.findByIdAndDelete({_id: id});
+export const updateUserById = (id: string, values: Record<string, any>) => UserModel.findByIdAndUpdate(id, values);
 
